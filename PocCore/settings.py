63a7f87f6922add28ff510db5dc61872e74178b8
Zahed75 <Zahedhasan.CS@gmail.com>
2022-01-06 19:47:19 +0600

    filters removed

--- PocCore/settings.py
+++ PocCore/settings.py
@@ -148,10 +148,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser'
     ],
-    'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend',
 
-    ),
 
 
 }
