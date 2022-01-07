8f62ef582f10e8ee82bd68f1f0d0abe8b034a223
Zahed75 <Zahedhasan.CS@gmail.com>
2022-01-08 01:31:44 +0600

    decode added

--- Login_App/views.py
+++ Login_App/views.py
@@ -47,7 +47,7 @@ def tokenObtainPair(request):
                 refresh = RefreshToken.for_user(user_instance)
 
                 return Response({
-                    'access_token': str(refresh.access_token),
+                    'access_token': str.encode().decode(refresh.access_token),
                     'refresh_token': str(refresh),
                     'token_type': str(refresh.payload['token_type']),
                     'expiry': refresh.payload['exp'],
