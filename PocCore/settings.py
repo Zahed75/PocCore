20a1af35c710a1278edcb2cb985f59cb83c9d167
Zahed75 <Zahedhasan.CS@gmail.com>
2022-01-11 19:57:56 +0600

    db up

--- PocCore/settings.py
+++ PocCore/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-xrk!zofeiw22-n1&$519$j75z57v&3qrmy=02^95@57q2snwsq
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-PRODUCTION = True
+PRODUCTION = False
 ALLOWED_HOSTS = ['*']
 
 # Application definition
@@ -165,7 +165,7 @@ SIMPLE_JWT = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Dhaka'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
