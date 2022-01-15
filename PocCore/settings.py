68ab31ed64279a01b1347820adfee7d814caf9e7
Zahed75 <Zahedhasan.CS@gmail.com>
2022-01-15 19:12:51 +0600

    db up

--- PocCore/settings.py
+++ PocCore/settings.py
@@ -235,9 +235,3 @@ CORS_ALLOW_HEADERS = [
     'x-csrftoken',
     'x-requested-with',
 ]
-
-
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
